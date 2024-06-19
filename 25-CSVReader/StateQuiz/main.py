757be31afe08c3848aaad9731e9a935b72cd52d7
Adrian Fraire-Vasquez <Adfrva@gmail.com>
2024-06-18 21:19:35 -0600

    Completing US state quiz game

--- 25-CSVReader/StateQuiz/main.py
+++ 25-CSVReader/StateQuiz/main.py
@@ -7,11 +7,31 @@ states_df = pandas.read_csv("50_states.csv")
 
 #Create turtle screen with blank US map image
 screen.addshape("blank_states_img.gif")
+screen.setup(height=481, width=725)
 turtle.shape("blank_states_img.gif")
 
-state_guess = screen.textinput(title="Guess a state", prompt="What's another state? ")
 
-#TODO Make if statement that checks if the state guessed is in the dataframe
-#If so, make state name display where the coordinates are on the map
+def check_guess(guess):
+    cap_guess = guess.title()
 
-turtle.mainloop()
+    if states_df['state'].str.contains(cap_guess).any():
+        row_match = states_df.loc[states_df['state'] == cap_guess]
+        x_coord = row_match['x'].item()
+        y_coord = row_match['y'].item()
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(x_coord, y_coord)
+        t.write(cap_guess)
+        print(cap_guess)
+
+
+end_quiz = 0
+while not end_quiz:
+    state_guess = screen.textinput(title="Guess a state", prompt="What's another state? ")
+
+    if state_guess.lower() == "exit":
+        end_quiz = 1
+        turtle.bye()
+    else:
+        check_guess(state_guess)
